fbaf49aa9c261e101b835bd72eeee8ae1d2d02dc
brantdong123 <brant.dong@hotmail.com>
2021-05-30 20:50:25 +0800

    first commit

---
+++ src/L131.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+//main idea: use manacher's algorithm ,get all odd palindromes & even
+// palindromes.
+// then the problem is to get minimum palindroms as possible
+public class L131 {
+    public List<List<String>> partition(String s) {
+        return null;
+    }
+}
